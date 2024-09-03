f78ccd551366530cfa146c916b984d219f566976
herr kaste <herr.kaste@gmail.com>
2024-09-03 15:47:20 +0200

    Fix regex for Windows' absolute paths
    Same: https://github.com/hsghori/SublimeLinter-contrib-cspell/pull/1

--- linter.py
+++ linter.py
@@ -4,6 +4,6 @@ from SublimeLinter.lint import Linter, STREAM_STDOUT
 class CSpell(Linter):
     cmd = 'cspell ${file_on_disk}'
     defaults = {'selector': 'source'}
-    regex = r'^(?P<filename>[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
+    regex = r'^(?P<filename>(\S:)?[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
     tempfile_suffix = '-'
     error_stream = STREAM_STDOUT
