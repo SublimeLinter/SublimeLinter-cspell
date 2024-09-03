d3a61942c30dc86c8a471a2f596a4d1cd1017b4b
herr kaste <herr.kaste@gmail.com>
2024-09-03 15:47:54 +0200

    Switch to background linting using temporary files

--- linter.py
+++ linter.py
@@ -2,8 +2,8 @@ from SublimeLinter.lint import Linter, STREAM_STDOUT
 
 
 class CSpell(Linter):
-    cmd = 'cspell ${file_on_disk}'
+    cmd = 'cspell ${temp_file}'
     defaults = {'selector': 'source'}
     regex = r'^(?P<filename>(\S:)?[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
-    tempfile_suffix = '-'
+    tempfile_suffix = '.'
     error_stream = STREAM_STDOUT
