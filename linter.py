57f59e35d1531d810d3b3457f4c87f0f030bc3aa
herr kaste <herr.kaste@gmail.com>
2025-06-20 19:58:19 +0200

    Let's enforce the default reporter

--- linter.py
+++ linter.py
@@ -2,7 +2,7 @@ from SublimeLinter.lint import Linter, STREAM_STDOUT
 
 
 class CSpell(Linter):
-    cmd = 'cspell stdin://${file}'
+    cmd = 'cspell --reporter default stdin://${file}'
     defaults = {'selector': 'source'}
     regex = r'^(?P<filename>(stdin:(///)?)?(\S:)?[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
     error_stream = STREAM_STDOUT
