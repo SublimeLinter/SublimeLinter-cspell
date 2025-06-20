4076ab17ca1f03bd6b4d216d8da48d5195f42e48
herr kaste <herr.kaste@gmail.com>
2025-06-20 19:57:33 +0200

    Fix "stdin:$file" actually lints the file on disk
    Interesting bug on cspell's side.  Using just the prefix "stdin:"
    silently strips that and runs on the file on disk.

--- linter.py
+++ linter.py
@@ -2,7 +2,7 @@ from SublimeLinter.lint import Linter, STREAM_STDOUT
 
 
 class CSpell(Linter):
-    cmd = 'cspell stdin:${file}'
+    cmd = 'cspell stdin://${file}'
     defaults = {'selector': 'source'}
     regex = r'^(?P<filename>(\S:)?[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
     error_stream = STREAM_STDOUT
