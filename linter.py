b4ede2faab7a07e481482e68c1b10741e1358c29
herr kaste <herr.kaste@gmail.com>
2025-06-20 19:12:15 +0200

    Let it run on stdin mode

--- linter.py
+++ linter.py
@@ -2,8 +2,7 @@ from SublimeLinter.lint import Linter, STREAM_STDOUT
 
 
 class CSpell(Linter):
-    cmd = 'cspell ${temp_file}'
+    cmd = 'cspell stdin:${file}'
     defaults = {'selector': 'source'}
     regex = r'^(?P<filename>(\S:)?[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
-    tempfile_suffix = '.'
     error_stream = STREAM_STDOUT
