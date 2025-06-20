a73e64eefab97c9fc7de37fb4c2d031eca586920
herr kaste <herr.kaste@gmail.com>
2025-06-20 19:55:47 +0200

    Workaround cspell bug when cwd contains spaces
    ... or probably any chars that gets percent encoded when transformed
    to a URI.
    
    cspell translates the given path to a URI but the reporter has a bug
    where it compares the normal working dir with the percent encoded one.
    These two never match obviously and then cspell prints the complete
    percent encoded URI as the filename.
    
    We thus (a) teach the regex about these stdin filenames, (b) teach
    `is_stdin_filename` to always return `True` because the filename
    the reporter reports is not a filename that can found on the filesystem
    but an URI.  If we return true, SublimeLinter core assumes the current
    view

--- linter.py
+++ linter.py
@@ -4,5 +4,9 @@ from SublimeLinter.lint import Linter, STREAM_STDOUT
 class CSpell(Linter):
     cmd = 'cspell stdin://${file}'
     defaults = {'selector': 'source'}
-    regex = r'^(?P<filename>(\S:)?[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
+    regex = r'^(?P<filename>(stdin:(///)?)?(\S:)?[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
     error_stream = STREAM_STDOUT
+
+    @staticmethod
+    def is_stdin_filename(filename: str) -> bool:
+        return True
